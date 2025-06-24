f58dfe17ae81352bff58cc9d2964e5ef2dcb49db
fforfachriza <dimasavrian346@gmail.com>
2025-06-24 22:43:57 +0700

    refactor: remove unused function in signin

--- src/app/auth/login/_components/login.tsx
+++ src/app/auth/login/_components/login.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React, { useRef } from "react";
+import type React from "react";
+import { useRef } from "react";
 import { signIn } from "next-auth/react";
 
 export default function LoginComponents() {
