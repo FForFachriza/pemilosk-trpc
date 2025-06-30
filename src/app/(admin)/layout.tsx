cf88da8f2c208151386a8a37eb2e22427ed81ec0
fforfachriza <dimasavrian346@gmail.com>
2025-06-30 22:00:09 +0700

    style: add theme switcher in sidebar navbar

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -14,29 +14,33 @@ import {
 	SidebarTrigger,
 } from "@/components/ui/sidebar";
 import type React from "react";
+import { ToggleTheme } from "@/components/toggle-theme";
 
 export default function Page({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
 			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-					<SidebarTrigger className="-ml-1" />
-					<Separator
-						orientation="vertical"
-						className="mr-2 data-[orientation=vertical]:h-4"
-					/>
-					<Breadcrumb>
-						<BreadcrumbList>
-							<BreadcrumbItem className="hidden md:block">
-								<BreadcrumbLink href="/">Home</BreadcrumbLink>
-							</BreadcrumbItem>
-							<BreadcrumbSeparator className="hidden md:block" />
-							<BreadcrumbItem>
-								<BreadcrumbPage>Dashboard</BreadcrumbPage>
-							</BreadcrumbItem>
-						</BreadcrumbList>
-					</Breadcrumb>
+				<header className="flex h-16 shrink-0 items-center justify-between border-b px-4">
+					<section className="flex items-center gap-2">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 data-[orientation=vertical]:h-4"
+						/>
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem className="hidden md:block">
+									<BreadcrumbLink href="/">Home</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator className="hidden md:block" />
+								<BreadcrumbItem>
+									<BreadcrumbPage>Dashboard</BreadcrumbPage>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</section>
+					<ToggleTheme />
 				</header>
 				<div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
 			</SidebarInset>
