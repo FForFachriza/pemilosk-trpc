9d734486a804a74540d8e6d6a4fceea3b982d76b
fforfachriza <dimasavrian346@gmail.com>
2025-07-08 23:03:51 +0700

    feat(be): get active periode

--- src/server/api/routers/periode.ts
+++ src/server/api/routers/periode.ts
@@ -1,9 +1,18 @@
-import { createTRPCRouter, adminProcedure } from "@/server/api/trpc";
+import {createTRPCRouter, adminProcedure, protectedProcedure} from "@/server/api/trpc";
 
 export const periodeRouter = createTRPCRouter({
-	getPeriodes: adminProcedure.query(async ({ ctx }) => {
-		const periode = await ctx.db.periode.findMany();
+    getPeriodes: adminProcedure.query(async ({ctx}) => {
+        const periode = await ctx.db.periode.findMany();
 
-		return periode ?? null;
-	}),
+        return periode ?? null;
+    }),
+    getActivePeriode: protectedProcedure.query(async ({ctx}) => {
+        const periode = await ctx.db.periode.findFirst({
+            where: {
+                isActive: true,
+            }
+        })
+
+        return periode ?? null;
+    })
 });
