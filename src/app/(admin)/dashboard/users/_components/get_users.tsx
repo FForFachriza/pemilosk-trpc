9ff12475a91d7dd956fc7574ea9de2e08569bbc4
fforfachriza <dimasavrian346@gmail.com>
2025-07-08 22:52:41 +0700

    style: remove unused comment in get users

--- src/app/(admin)/dashboard/users/_components/get_users.tsx
+++ src/app/(admin)/dashboard/users/_components/get_users.tsx
@@ -1,20 +1,19 @@
-// GetUsers.tsx
 "use client";
-import { api } from "@/trpc/react";
-import { DataTable } from "@/app/(admin)/dashboard/users/_components/data-table";
-import { createUsersColumns } from "@/app/(admin)/dashboard/users/_components/columns";
-import { parseAsString, useQueryState } from "nuqs";
+import {api} from "@/trpc/react";
+import {DataTable} from "@/app/(admin)/dashboard/users/_components/data-table";
+import {createUsersColumns} from "@/app/(admin)/dashboard/users/_components/columns";
+import {parseAsString, useQueryState} from "nuqs";
 
 export function GetUsers() {
-	const [periodeId] = useQueryState("periode", parseAsString);
+    const [periodeId] = useQueryState("periode", parseAsString);
 
-	const [users] = api.user.getUsers.useSuspenseQuery({
-		periodeId: periodeId ?? undefined,
-	});
+    const [users] = api.user.getUsers.useSuspenseQuery({
+        periodeId: periodeId ?? undefined,
+    });
 
-	const [periodes] = api.periode.getPeriodes.useSuspenseQuery();
+    const [periodes] = api.periode.getPeriodes.useSuspenseQuery();
 
-	const columns = createUsersColumns(periodes);
+    const columns = createUsersColumns(periodes);
 
-	return <DataTable columns={columns} periode={periodes} data={users} />;
+    return <DataTable columns={columns} periode={periodes} data={users}/>;
 }
