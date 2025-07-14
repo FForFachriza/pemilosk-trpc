2c8c775a3962f5b9259933fbefda0c0637e700bf
fforfachriza <dimasavrian346@gmail.com>
2025-07-15 00:25:21 +0700

    refactor: use link breadcrumbs component in layout for dashboard

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,20 +1,12 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
 	SidebarInset,
 	SidebarProvider,
 	SidebarTrigger,
 } from "@/components/ui/sidebar";
 import type React from "react";
 import { ToggleTheme } from "@/components/toggle-theme";
+import LinkBreadcrumbs from "@/components/link-breadcrumbs";
 
 export default function Page({ children }: { children: React.ReactNode }) {
 	return (
@@ -24,21 +16,7 @@ export default function Page({ children }: { children: React.ReactNode }) {
 				<header className="flex h-16 shrink-0 items-center justify-between border-b px-4">
 					<section className="flex items-center gap-2">
 						<SidebarTrigger className="-ml-1" />
-						<Separator
-							orientation="vertical"
-							className="mr-2 data-[orientation=vertical]:h-4"
-						/>
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="/">Home</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator className="hidden md:block" />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Dashboard</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
+						<LinkBreadcrumbs />
 					</section>
 					<ToggleTheme />
 				</header>
